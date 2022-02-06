6bc2cebe0f81e7481d3fe825ee46501b494d4fcd
jovanamilojevic <jovanan834@gmail.com>
2022-02-06 01:20:26 +0100

    facilities

---
+++ facilities.php
@@ -0,0 +1,225 @@
+<?php
+include_once('header.php');
+
+ ?>
+
+
+		<!-- One -->
+			<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Minex Apartments</p>
+						<h2>Facilities</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="box">
+
+
+
+            <div class="content">
+              <header class="align-center">
+                <p>Hercegovina Winery</p>
+                <h2>Wine</h2>
+              </header>
+
+              <!-- Elements -->
+              <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                  <!-- <h3>Locations:</h3> -->
+                  <span class="image fit" height="300px" ><img src="images/Vine Trebinje.jpg" alt="" /></span>
+                  </br>
+                </div>
+                <!-- Right section -->
+                <div class="6u$ 12u$(medium)">
+                  <p>"Wine is a living thing. It is made, not only of grapes and yeasts, but of skill and patience. When drinking it remember that to the making of that wine has gone, not only the labor and care of years, but the experience of centuries." - Allan Sichel <br> Selection of best Herzegovina vine:
+                  <div class="row">
+
+                    <div class="u$ align-center">
+                      <ul class="icons">
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebdook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-google"><span class="label">Google</span></a></li>
+                      </ul>
+
+                      <ul class="actions fit">
+                        <!-- <li><a href="#" class="button special fit">Directions</a></li> -->
+                        <!-- <li><a href="#" class="button fit">Fit</a></li> -->
+                        <li><a href="http://minexapartments.com/prices.php" class="button alt fit">Prices</a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div class="content">
+							<header class="align-center">
+								<p>Cycling</p>
+								<h2>Yellow Bicycles - Free</h2>
+							</header>
+              <!-- <h2 id="elements">Elements</h2> -->
+
+              <!-- Elements -->
+              <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                  <!-- <h3>Locations:</h3> -->
+                  <span class="image fit" height="300px" ><img src="images/Free Bicycles - Minex Apartments.jpg" alt="" /></span>
+                  </br>
+                </div>
+                <!-- Right section -->
+                <div class="6u$ 12u$(medium)">
+                  <!-- <p>Free bicycles good for short distance. -->
+                    <!-- <br>Yellow bicycle good way to get to local store, buy some fresh organic vegetables in neighborhood or visit Monastery Tvrdos, grab glasses and wine and find quite plase next to the river Trebisnjica. -->
+                    <!-- Every time I see an adult on a bicycle, I no longer despair for the future of the human race. -->
+                    <p>"I want to take time for myself, because all my life is ta-ta-ta-ta. So I don't want nothing special, just to breathe. I'd like to work maybe a half day and then take my bicycle and go by the riverside."" - Renzo Rosso
+                    <br>Yellow bicycle - good way to get to local store, buy some fresh organic vegetables from neighborhood visit a Monastery or get bottle of good local wine and chill out by the river!
+
+                  <div class="row">
+
+                    <div class="u$ align-center">
+                      <ul class="icons">
+                        <!-- <li><a href="#" class="button special">Direction</a></li> -->
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebdook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-google"><span class="label">Google</span></a></li>
+                      </ul>
+
+                      <ul class="actions fit">
+                        <!-- <li><a href="#" class="button special fit">Directions</a></li> -->
+                        <!-- <li><a href="#" class="button fit">Fit</a></li> -->
+                        <li><a href="http://minexapartments.com/prices.php" class="button alt fit">Prices</a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          <!-- </div> -->
+
+
+
+
+            <div class="content">
+							<header class="align-center">
+								<p>Cycling</p>
+								<h2>Rent a Bicycles</h2>
+							</header>
+              <!-- <h2 id="elements">Elements</h2> -->
+
+              <!-- Elements -->
+              <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                  <!-- <h3>Locations:</h3> -->
+                  <span class="image fit" height="300px" ><img src="images/Rent a Bicycle II.jpg" alt="" /></span>
+                  </br>
+
+                </div>
+                <!-- Right section -->
+                <div class="6u$ 12u$(medium)">
+                  <p>
+                    <!-- <br>The best way to explore area include Trebinje town, monastery Tvrdos, restaurants, grab new experience. -->
+                    <!-- <br>I want to take time for myself, because all my life is ta-ta-ta-ta. So I don't want nothing special, just to breathe. I'd like to work maybe a half day and then take my bicycle and go by the riverside. -->
+                    "Riding a bicycle is the summit of human endeavour - an almost neutral environmental effect coupled with the ability to travel substantial distances without disturbing anybody. The bike is the perfect marriage of technology and human energy." - Jeremy Corbyn
+
+                    <br>Low weight bicycles, good option to discover Trebinje and area.
+                  <div class="row">
+
+                    <div class="u$ align-center">
+                      <ul class="icons">
+                        <!-- <li><a href="#" class="button special">Direction</a></li> -->
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebdook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-google"><span class="label">Google</span></a></li>
+                      </ul>
+
+
+                      <ul class="actions fit">
+                        <!-- <li><a href="#" class="button special fit">Directions</a></li> -->
+                        <!-- <li><a href="#" class="button fit">Fit</a></li> -->
+                        <li><a href="http://minexapartments.com/prices.php" class="button alt fit">Prices</a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div class="content">
+							<header class="align-center">
+								<p>Keep calm and love hookah...</p>
+								<h2>Shisha - Hookah</h2>
+							</header>
+              <!-- <h2 id="elements">Elements</h2> -->
+
+              <!-- Elements -->
+              <div class="row 200%">
+                <div class="6u 12u$(medium)">
+                  <!-- <h3>Locations:</h3> -->
+                  <span class="image fit" height="300px" ><img src="images/Hookah - Minex Apartments.jpg" alt="" /></span>
+
+                  <!-- Cycling - Minex Apartmnets - Monastery Tvrdos.jpg -->
+
+                  </br>
+
+                </div>
+                <!-- Right section -->
+                <div class="6u$ 12u$(medium)">
+                  <p> "Smoking a hookah is nothing like smoking a cigarette...<br>
+                      cigarettes are for nervous people, competitive people, people on the run. When you smoke a hookah, you have time to think. It teaches you patience and tolerance, and gives you an appreciation of good company." - Ismet Ertep <br>Several different flavours for all moods,<br>
+                  <div class="row">
+
+                    <div class="u$ align-center ">
+                      <ul class="icons">
+                        <!-- <li><a href="#" class="button special">Direction</a></li> -->
+                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebdook</span></a></li>
+                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon fa-google"><span class="label">Google</span></a></li>
+                      </ul>
+
+
+                      <ul class="actions fit">
+                        <!-- <li><a href="#" class="button special fit">Directions</a></li> -->
+                        <!-- <li><a href="#" class="button fit">Fit</a></li> -->
+                        <li><a href="http://minexapartments.com/prices.php" class="button alt fit">Prices</a></li>
+                      </ul>
+
+
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+
+
+
+<?php
+	include_once('footer.php');
+
+?>
